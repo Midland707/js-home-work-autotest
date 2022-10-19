b0b95b248c91306547729859678c1734da9607b8
Midland707 <rus14job@ukr.net>
2022-10-20 00:55:28 +0300

    add 31-32 autotest

--- mod1-auto31.js
+++ mod1-auto31.js
@@ -1,1 +1,10 @@
 //MODUL1 AUTOTEST31
+
+const courseTopic = "JavaScript essentials";
+// Change code below this line
+const courseTopicLength = courseTopic.length;
+ console.log("courseTopicLength =" + courseTopicLength);
+const firstElement = courseTopic[0];
+ console.log("firstElement=" + firstElement);
+const lastElement = courseTopic[courseTopic.length-1];
+ console.log("lastElement =" + lastElement);
--- mod1-auto32.js
+++ mod1-auto32.js
@@ -1,1 +1,13 @@
 //MODUL1 AUTOTEST32
+
+function getSubstring(string, length) {
+  const substring = string.slice(0, length) ; // Change this line
+  console.log(string + "select - " + substring);
+  return substring;
+}
+// Оголошена функція getSubstring(string, length)
+getSubstring("Hello world", 3); // повертає "Hel"
+getSubstring("Hello world", 6); // повертає "Hello"
+getSubstring("Hello world", 8); // повертає "Hello wo"
+getSubstring("Hello world", 11); // повертає "Hello world"
+getSubstring("Hello world", 0); // повертає ""
