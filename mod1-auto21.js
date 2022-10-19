dd86e3045800aa063f86eaff41ab29f519a8f485
Midland707 <rus14job@ukr.net>
2022-10-19 18:00:52 +0300

    add 21 autotest

--- mod1-auto21.js
+++ mod1-auto21.js
@@ -1,1 +1,12 @@
 //MODUL1 AUTOTEST21
+
+function isNumberInRange(start, end, number) {
+  const isInRange = start < number && end > number; // Change this line
+  console.log(isInRange);
+  return isInRange;
+}
+
+isNumberInRange(10, 30, 17); //true
+isNumberInRange(10, 30, 5); //false
+isNumberInRange(20, 50, 24); //true
+isNumberInRange(20, 50, 76); //false
